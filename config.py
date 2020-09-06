bcd9e5e17dee24c562f41864766ee485bf8fbab3
nicolaskiprop <yegonicholas@gmail.com>
2020-09-06 20:31:57 +0300

    sessions

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 # default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = 'skodo'
+    SECRET_KEY = '4E\xdfIm\x08\xbc*i\xf2\x8b\xd6\xb7\xbb\x1b\x90\x84\xe0\x91\xa7\xdf\xfc\xfd\x9f'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
